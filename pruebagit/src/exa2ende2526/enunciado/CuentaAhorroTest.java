50f35042f1cabdd0fd5ef6da6806b041de2a52aa
Ricardo <ricardo04172007@gmail.com>
2026-06-19 03:44:29 +0200

    Clase Test para CuentaAhorro

---
+++ pruebagit/src/exa2ende2526/enunciado/CuentaAhorroTest.java
@@ -0,0 +1,62 @@
+package exa2ende2526.enunciado;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.*;
+import org.junit.runners.Parameterized.Parameters;
+import java.util.*;
+
+public class CuentaAhorroTest {
+	private static List<CuentaBancaria> listaCuentas; // lista compartida entre tests
+
+	private double saldoInicial;
+	private double tasaInteres;
+	private double interesEsperado;
+	private CuentaAhorro cuenta;
+
+	public CuentaAhorroTest(double saldoInicial, double tasaInteres, double interesEsperado, CuentaAhorro cuenta) {
+		this.saldoInicial = saldoInicial;
+		this.tasaInteres = tasaInteres;
+		this.interesEsperado = interesEsperado;
+		this.cuenta = cuenta;
+	}
+
+	@Parameters
+	public static Collection<Object[]> datos() {
+		return Arrays.asList(new Object[][] { { 1000, 10, -10 }, { 500, 5, -5 }, { 2000, 20, -20 }, { 1500, 0, 0 },
+				{ 0, 15, -15 } });
+	}
+
+	@BeforeClass
+	public static void iniciarlistaCuentas() {
+		listaCuentas = new ArrayList<CuentaBancaria>();
+		System.out.println("=== Creando lista de cuentas para todos los tests ===");
+	}
+
+	@Before
+	public void crearTipoCuenta() {
+		CuentaAhorro cuenta = new CuentaAhorro("Test", saldoInicial, tasaInteres);
+	}
+
+	@AfterClass
+	public void limpiarLista() {
+		listaCuentas = null;
+		System.out.println("=== Lista de cuentas liberada despu�s de todos los tests ===");
+	}
+
+	@After
+	public void limpiarTipoCuenta() {
+		cuenta = null;
+	}
+
+	@Test
+	public void testCalcularInteres() {
+		assertEquals(interesEsperado, cuenta.calcularInteres(), 0.01);
+	}
+
+	@Ignore("Test ignorado")
+	public void testFiscal() {
+
+	}
+}
--- pruebagit/src/module-info.java
+++ pruebagit/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module pruebagit {
+	requires junit;
 }
