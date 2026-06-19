72d4b4fbb29aab53e3f310aeffacd73bc474f266
Ricardo <ricardo04172007@gmail.com>
2026-06-19 03:30:15 +0200

    modificacion en la clase CuentaBancaria

--- pruebagit/src/exa2ende2526/enunciado/CuentaBancaria.java
+++ pruebagit/src/exa2ende2526/enunciado/CuentaBancaria.java
@@ -25,12 +25,19 @@ public abstract class CuentaBancaria {
     }
 
     public void ingresar(double cantidad) {
-
+    	if(cantidad<0) {
+    		throw new IllegalArgumentException("No se pueden ingresar cantidades negativas");
+    	}
         saldo += cantidad;
     }
 
     public void reintegrar(double cantidad) {
-
+    	if(cantidad<0) {
+    		throw new IllegalArgumentException("No se sacar cantidades negativas");
+    	}
+    	if(saldo<cantidad) {
+    		throw new IllegalArgumentException("No se puede sacar más dinero que tu saldo actual");
+    	}
         saldo -= cantidad;
     }
 
